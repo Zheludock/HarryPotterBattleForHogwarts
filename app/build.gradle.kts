86b87c9e216b793ca29993fd55aa20cb37939454
Mikhail <129256994+Zheludock@users.noreply.github.com>
2024-11-15 14:49:27 +1000

    try 3, looksbetter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.harrypotterbattleforhogwarts"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.harrypotterbattleforhogwarts"
